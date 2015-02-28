6e7b4b6ecbe07d65effdb461b35e7ba9f09946df
rogeryee <roger.yee@hotmail.com>
2015-02-28 14:11:24 +0800

    Add new project for learning Swift grammar

---
+++ Swift_Study/Swift_Study/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_Study
+//
+//  Created by Roger Yee on 2/28/15.
+//  Copyright (c) 2015 Roger Yee. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
