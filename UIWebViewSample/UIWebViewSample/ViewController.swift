0a6abd5505c819bd876085794170ea0fc63571e5
rogeryee <roger.yee@hotmail.com>
2015-05-04 11:06:25 +0800

    Single WebView sample

--- UIWebViewSample/UIWebViewSample/ViewController.swift
+++ UIWebViewSample/UIWebViewSample/ViewController.swift
@@ -8,16 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate,UITextFieldDelegate {
 
-    @IBOutlet weak var loadType: UISegmentedControl!
+    @IBOutlet weak var textfield: UITextField!
     @IBOutlet weak var webview: UIWebView!
+    @IBOutlet weak var activitor: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        loadType.selectedSegmentIndex = 0;
-        typeChanged(loadType)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,10 +24,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func typeChanged(sender: UISegmentedControl) {
-        
-        
+    // Loading URL
+    func loadURL(urlStr:String, webview:UIWebView) {
+        let url = NSURL(string:("http://"+urlStr))
+        let request = NSURLRequest(URL: url!)
+        webview.loadRequest(request)
     }
-
+    
+    // 加载网页开始
+    func webViewDidStartLoad(webView: UIWebView) {
+        activitor.startAnimating()
+        
+        // 状态栏上的网络齿轮
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+    
+    // 加载网页结束
+    func webViewDidFinishLoad(webView: UIWebView) {
+        activitor.stopAnimating()
+        
+        // 状态栏上的网络齿轮
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        loadURL(textfield.text, webview: webview)
+        // 收起键盘
+        textfield.resignFirstResponder()
+        return true
+    }
+    
 }
 
