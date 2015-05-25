fe27370d96e284dc963c4c8ac3992962450a63b4
rogeryee <roger.yee@hotmail.com>
2015-05-25 12:11:56 +0800

    Add buttons to the customized tab bar

--- TabBar_NavigationSample/TabBar_NavigationSample/TabBarViewController.swift
+++ TabBar_NavigationSample/TabBar_NavigationSample/TabBarViewController.swift
@@ -13,6 +13,7 @@ class TabBarViewController : UITabBarController {
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         
+        // 隐藏系统自带的TabBar
         self.tabBar.hidden = true
     }
 
@@ -21,19 +22,46 @@ class TabBarViewController : UITabBarController {
     }
     
     override func viewDidLoad() {
+        // 加载自定义的TabBar
         loadCustomTabBar()
     }
     
     func loadCustomTabBar() {
         
-        var bgView = UIImageView()
-        bgView.image = UIImage(named: "tabBar")
+        var bgView = UIView()
+        bgView.backgroundColor = UIColor.grayColor()
         bgView.setTranslatesAutoresizingMaskIntoConstraints(false)
         self.view.addSubview(bgView)
         
-        let views = ["bg":bgView]
+        var msgBtn = UIButton()
+        msgBtn.tag = 0
+        msgBtn.setBackgroundImage(UIImage(named: "bubble"), forState: UIControlState.Normal)
+        msgBtn.setTranslatesAutoresizingMaskIntoConstraints(false)
+        msgBtn.addTarget(self, action: Selector("msg:"), forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(msgBtn)
+        
+        var downloadBtn = UIButton()
+        downloadBtn.tag = 1
+        downloadBtn.setBackgroundImage(UIImage(named: "cloud-download"), forState: UIControlState.Normal)
+        downloadBtn.setTranslatesAutoresizingMaskIntoConstraints(false)
+        downloadBtn.addTarget(self, action: Selector("download:"), forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(downloadBtn)
+        
+        let views = ["bg":bgView,"msg":msgBtn,"download":downloadBtn]
         self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[bg]|", options: NSLayoutFormatOptions.allZeros, metrics: nil, views: views))
         self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[bg(==70)]|", options: NSLayoutFormatOptions.allZeros, metrics: nil, views: views))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[msg]-20-|", options: NSLayoutFormatOptions.allZeros, metrics: nil, views: views))
+        self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:[download]-20-|", options: NSLayoutFormatOptions.allZeros, metrics: nil, views: views))
         
+        self.view.addConstraint(NSLayoutConstraint(item: msgBtn, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterX, multiplier: 0.5, constant: 0.0))
+        self.view.addConstraint(NSLayoutConstraint(item: downloadBtn, attribute: NSLayoutAttribute.CenterX, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.CenterX, multiplier: 1.5, constant: 0.0))
+    }
+    
+    func msg(button:UIButton) {
+        self.selectedIndex = button.tag
+    }
+    
+    func download(button:UIButton) {
+        self.selectedIndex = button.tag
     }
 }
